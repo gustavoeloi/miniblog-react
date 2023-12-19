c931f440acfa5b9e500a609a82f4e792bbfb890f
gustavooeloi <gustavooeloi@gmail.com>
2023-12-19 19:22:59 -0300

    style create post form

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,7 +1,74 @@
 import styles from "./CreatePost.module.css";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/authContext";
+
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("");
+  const [tags, setTags] = useState("");
+
+  return (
+    <div className={styles.create_post}>
+      <h1>Compartilhe seu momento!</h1>
+      <p>
+        Sabe aquele momento especial no seu coração? compartilhe com seus amigos
+      </p>
+
+      <form onSubmit={handleSubmit} className={styles.form_post}>
+        <label>
+          <span>Título</span>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            placeholder="Pense naquele título legal..."
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Descrição</span>
+          <textarea
+            name="body"
+            value={body}
+            placeholder="Descreva como foi esse momento especial para você"
+            onChange={(e) => e.target.value}
+          ></textarea>
+        </label>
+
+        <label>
+          <span>Imagem</span>
+          <input
+            type="text"
+            name="image"
+            value={image}
+            placeholder="Cole o link da imagem"
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Tags</span>
+          <input
+            type="text"
+            name="tags"
+            value={tags}
+            placeholder="Coloque as tags separadas por vírgula"
+            onChange={(e) => setTags(e.target.value)}
+          />
+        </label>
+
+        <button>Postar</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePost;
